4a414cfbc50de023b638781947223979e82c19d7
KeilAwk <82655578+KeilAwk@users.noreply.github.com>
2026-02-13 09:38:59 +0800

    updated.

---
+++ sources/HavenM.ACSetting/src/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+
+namespace HavenM
+{
+    /// <summary>
+    /// ::: HavenM
+    /// ::: Licensed under MIT
+    /// ::: Powerer by RavenfieldCommmunity
+    /// </summary>
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Main : BaseUnityPlugin
+    {
+        public ConfigEntry<bool> thisisABool;
+        private void Awake()
+        {
+            logger = Logger;
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            //register config: tag, name, value, description
+            thisisABool = Config.Bind<bool>("General", "thisisABool", true, "Enable plugin?");
+            //register config update solver
+            Config.SettingChanged += this.OnSettingChanged;
+
+            UnionPatch.Patch();
+        }
+
+        //Event subscriber to solve plugin configs changes
+        private void OnSettingChanged(object sender, SettingChangedEventArgs e)
+        {
+            
+        }
+    }
+}
